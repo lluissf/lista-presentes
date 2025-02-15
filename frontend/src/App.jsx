df65b8082a66ec61c667e6233a55faa06132a1fa
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2025-02-15 16:19:33 -0300

    adicionado novo texto ao entrar no site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,9 +56,17 @@ export default function App() {
 
   return (
     <div className="min-h-screen p-6 flex flex-col items-center">
-      <h1 className="text-4xl font-bold text-green-700 mb-6">
-        Lista de Presentes
+      <h1 className="text-4xl font-bold text-green-700 mb-6 text-center bg-white">
+        Bem vindo à Lista de Presentes para o chá de
       </h1>
+      <h1 className="text-5xl font-bold text-green-700 mb-6 bg-white">
+        Letícia e Luian
+      </h1>
+      <p className="text-lg text-gray-600 mb-6">
+        {" "}
+        Marque os presentes que você comprou clicando no botão abaixo de cada
+        presente.{" "}
+      </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-4xl">
         {gifts.map((gift) => (
           <div
